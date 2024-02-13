d8ad86b3f710914c332bbd9b9547a81dac4ea6e2
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-12 19:57:32 -0800

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Rodion Akhmedov on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
