0cc4cd946def97a39914b8e8338a86e6fc8cfbf9
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 21:00:20 -0800

    Appetizer Cell View

--- Appetizer/Views/Screens/Appetizer/AppetizerCell.swift
+++ Appetizer/Views/Screens/Appetizer/AppetizerCell.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct AppetizerCell: View {
+    
+    var appetizer: Appetizer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Image(.foodPlaceholder)
+                    .resizable()
+            }
+            .clipShape(.rect(cornerRadius: 10, style: .continuous))
+            .frame(width: 120, height: 90)
+            .aspectRatio(contentMode: .fill)
+            
+            
+            
+            VStack(alignment: .leading) {
+                Text(appetizer.name)
+                    .font(.subheadline)
+                Text(appetizer.price, format: .currency(code: "USD"))
+                    .foregroundStyle(.gray)
+            }
+        }
     }
 }
 
 #Preview {
-    AppetizerCell()
+    AppetizerCell(appetizer: Appetizer(id: 1, name: "Asian Flake Stake", description: "This perfectly thin cut just melts in your mouth", imageURL: "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg", carbs: 6, protein: 12, calories: 340, price: 9.99))
 }
