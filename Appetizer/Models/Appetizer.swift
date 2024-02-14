50a047a936e10a997ff7bd005d10258e9bab3f77
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 20:56:18 -0800

    Appetizer model

--- Appetizer/Models/Appetizer.swift
+++ Appetizer/Models/Appetizer.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Appetizer: Identifiable, Codable {
+    var id: Int
+    var name: String
+    var description: String
+    var imageURL: String
+    var carbs: Int
+    var protein: Int
+    var calories: Int
+    var price: Double
+}
+
+struct AppetizerResponse: Codable {
+    var request = [Appetizer]()
+}
