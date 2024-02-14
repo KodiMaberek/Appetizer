73f57b7aaae72513e1a26a7a906578aa667d259e
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 20:57:09 -0800

    Network Manager on async awayt

--- Appetizer/Remote/NetworkManager.swift
+++ Appetizer/Remote/NetworkManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum URlError: Error {
+    case invalideResponse
+    case unabledToContinue
+}
+
+final class NetworkManager {
+    
+    static let manager = NetworkManager()
+    private init() {}
+    
+    func loadData() async throws -> [Appetizer] {
+        let (data, response) = try await URLSession.shared.data(from: .mainUrl)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw URlError.invalideResponse
+        }
+        
+        let decoded = try JSONDecoder().decode(AppetizerResponse.self, from: data)
+        return decoded.request
+    }
+}
