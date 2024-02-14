ed0a99692332a04a5db7a4169872a766ecec2c67
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 21:00:40 -0800

    Appetier List

--- Appetizer/Views/Screens/Appetizer/AppetizerList.swift
+++ Appetizer/Views/Screens/Appetizer/AppetizerList.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct AppetizerList: View {
+    
+    @StateObject var vm = AppetizerViewModel()
+    
+    @State private var showingDetails = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                List(vm.appetizer) { appetizer in
+                    AppetizerCell(appetizer: appetizer)
+                        .onTapGesture {
+                            withAnimation {
+                                vm.appetizerSelect = appetizer
+                                showingDetails = true
+                            }
+                        }
+                }
+                .blur(radius: showingDetails ? 8 : 0)
+                .disabled(showingDetails ? true : false)
+            }
+            .showingDetails(showingDetail: $showingDetails, appetizer: vm.appetizerSelect)
+            .isLoadingView(loading: vm.isLoading)
+            .task {
+                await vm.load()
+            }
+            .alert(item: $vm.alert, content: { alert in
+                Alert(title: alert.title, message: alert.description, dismissButton: alert.button)
+            })
+            .navigationTitle("Appetizer 🍟")
+            
+        }
     }
 }
 
