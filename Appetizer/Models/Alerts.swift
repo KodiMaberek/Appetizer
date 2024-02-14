321de76d6ffb77ee159a7d8b7bd2eaf6fe428354
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 20:55:57 -0800

    Alert for all kind of notification

--- Appetizer/Models/Alerts.swift
+++ Appetizer/Models/Alerts.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    var id = UUID()
+    var title: Text
+    var description: Text
+    var button: Alert.Button
+}
+
+struct AlertContext {
+    //MARK: Url alert
+    let invalideResponse = AlertItem(title: Text("Server Error"), description: Text("Cannot connect from the server. Please try later or connect support"), button: .default(Text("OK")))
+    
+    let unabledToComplete = AlertItem(title: Text("Error"), description: Text("Unabled to complete. Please check your internet connection"), button: .default(Text("OK")))
+    
+    
+}
