698b21e1832fc0608e9750d9fedb487f85d694c0
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-13 21:01:09 -0800

    Appetizer ViewModel

--- Appetizer/Views/Screens/Appetizer/AppetizerViewModel.swift
+++ Appetizer/Views/Screens/Appetizer/AppetizerViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+
+final class AppetizerViewModel: ObservableObject {
+    
+    @Published var appetizer = [Appetizer]()
+    
+    let shared = NetworkManager.manager
+    
+    @Published var isLoading = false
+    @Published var alert: AlertItem?
+    @Published var appetizerSelect: Appetizer?
+    func load() async {
+        isLoading = true
+        do {
+            let request = try await shared.loadData()
+            appetizer = request
+        } catch {
+            alert = AlertContext().unabledToComplete
+            print(error.localizedDescription)
+        }
+        isLoading = false
+    }
+}
+
