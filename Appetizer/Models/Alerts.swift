4e6bed1e9f5eb50fc3d546af1ba5e2308f369dc2
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-15 13:13:43 -0800

    Update Alerts

--- Appetizer/Models/Alerts.swift
+++ Appetizer/Models/Alerts.swift
@@ -16,10 +16,20 @@ struct AlertItem: Identifiable {
 }
 
 struct AlertContext {
-    //MARK: Url alert
+    //MARK: URL alert
     let invalideResponse = AlertItem(title: Text("Server Error"), description: Text("Cannot connect from the server. Please try later or connect support"), button: .default(Text("OK")))
     
     let unabledToComplete = AlertItem(title: Text("Error"), description: Text("Unabled to complete. Please check your internet connection"), button: .default(Text("OK")))
     
+    //MARK: Account alert
+    let invalidFirstName = AlertItem(title: Text("Missing first name"), description: Text("Please enter your first name"), button: .default(Text("OK")))
     
+    let invalidLastname = AlertItem(title: Text("Missing last name"), description: Text("Please enter your first name"), button: .default(Text("OK")))
+    
+    let invalidEmail = AlertItem(title: Text("Invalide email"), description: Text("Please enter correct email"), button: .default(Text("OK")))
+    
+    //MARK: Save data
+    let saveUser = AlertItem(title: Text("Profile Saved"), description: Text("You new information was success save"), button: .default(Text("OK")))
+    
+    let errorSave = AlertItem(title: Text("Profile Error"), description: Text("Couldn't save new information"), button: .default(Text("OK")))
 }
