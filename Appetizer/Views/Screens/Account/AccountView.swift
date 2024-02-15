6cbe1c45f39bfdc4271be63b908144e3d7aa50f7
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-15 13:17:27 -0800

    Account Screen

--- Appetizer/Views/Screens/Account/AccountView.swift
+++ Appetizer/Views/Screens/Account/AccountView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @StateObject var vm = AccountViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section("personal info") {
+                    TextField("First name", text: $vm.person.firstName)
+                    TextField("Last name", text: $vm.person.lastName)
+                    TextField("Email", text: $vm.person.email)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.emailAddress)
+                        .textContentType(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Your birthday", selection: $vm.person.birthDate, displayedComponents: .date)
+                    Button {
+                        vm.saveChange()
+                    } label: {
+                        Text("Save changes")
+                    }
+                    
+                }
+                Section("Requests") {
+                    Toggle("Extra napkins", isOn: $vm.person.extraNapkins)
+                    Toggle("Frequent order", isOn: $vm.person.freqencyOrder)
+                }
+        
+            }
+            .alert(item: $vm.alert, content: { alert in
+                Alert(title: alert.title, message: alert.description, dismissButton: alert.button)
+            })
+            .onAppear {
+                vm.retrieving()
+            }
+            .tint(.brand)
+                .navigationTitle("Account🤓")
+        }
     }
 }
 
