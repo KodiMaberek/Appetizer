1c4b1a9dae063f01b019ce3c7b994504acc4ab97
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-19 11:23:25 -0800

    Order View

--- Appetizer/Views/Screens/Order/OrderView.swift
+++ Appetizer/Views/Screens/Order/OrderView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @EnvironmentObject var vm: OrderViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            NavigationView {
+                List {
+                    ForEach(vm.order.items) { order in
+                        AppetizerCell(appetizer: order)
+                    }
+                    .onDelete(perform: vm.delete)
+                }
+                .disabled(vm.order.items.isEmpty ? true : false)
+                .navigationTitle("Order 🧾")
+            }
+            if vm.order.items.isEmpty {
+                OrderIsEmpty()
+                    .offset(y: 200)
+            }
+        }
     }
 }
 
