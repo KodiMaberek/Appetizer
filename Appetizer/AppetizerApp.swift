1f7ee828ba821b9d855374362b5277fbe7cec2c7
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-19 11:25:55 -0800

    AppetizerApp

--- Appetizer/AppetizerApp.swift
+++ Appetizer/AppetizerApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct AppetizerApp: App {
+    
+ var order = OrderViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppetizerTabView().environmentObject(order)
         }
     }
 }
