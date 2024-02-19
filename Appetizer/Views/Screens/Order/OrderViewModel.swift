6089aa0eaa834389cf4db4d6ee1e35188e9d1cdc
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-19 11:23:46 -0800

    Order ViewModel

--- Appetizer/Views/Screens/Order/OrderViewModel.swift
+++ Appetizer/Views/Screens/Order/OrderViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+final class OrderViewModel: ObservableObject {
+    @Published var order = Order()
+    
+    func add(_ appertizer: Appetizer) {
+        order.items.append(appertizer)
+    }
+    
+    func delete(at offset: IndexSet) {
+        order.items.remove(atOffsets: offset)
+    }
+}
