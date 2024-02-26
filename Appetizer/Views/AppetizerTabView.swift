da2b04a86ff2e06fd39827614b80cf6fa9e1fa6a
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-25 22:24:03 -0800

    Added index for toggle every screen

--- Appetizer/Views/AppetizerTabView.swift
+++ Appetizer/Views/AppetizerTabView.swift
@@ -8,23 +8,29 @@
 import SwiftUI
 
 struct AppetizerTabView: View {
+    
+    @EnvironmentObject var tabIndex: TabBar
     var body: some View {
-        TabView {
+        
+        TabView(selection: $tabIndex.selectIndex) {
             AppetizerList()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
+                .tag(0)
             AccountView()
                 .tabItem {
                     Image(systemName: "person")
                     Text("Account")
                 }
-            OrderView()
-                .tabItem {
-                    Image(systemName: "bag")
-                    Text("Order")
-                }
+                .tag(1)
+//            OrderView()
+//                .tabItem {
+//                    Image(systemName: "bag")
+//                    Text("Order")
+//                }
+//                .tag(2)
         }
         .tint(.brand)
     }
