b984f0a683a76cd1b73cc2e2345c1233e42d6870
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-25 22:38:03 -0800

    Add button

--- Appetizer/Views/Screens/Buttons/AddButton.swift
+++ Appetizer/Views/Screens/Buttons/AddButton.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AddButton: View {
+    @Environment(\.colorScheme) var colorScheme
     
     var title: LocalizedStringKey
     
@@ -16,13 +17,16 @@ struct AddButton: View {
             Text(title).fontWeight(.semibold)
                 .font(.title2)
                 .frame(width: 250, height: 45)
-                .foregroundStyle(.white)
-                .background(Color(.brand))
+                .background(colorScheme == .dark ? .gray : .brand)
+                .foregroundStyle(colorScheme == .dark ? .brand : .white)
                 .clipShape(.rect(cornerRadius: 10, style: .continuous))
         }
     }
 }
 
 #Preview {
-    AddButton(title: "$ 9.9 - add to order")
+    Group {
+        AddButton(title: "$ 9.9 - add to order")
+            .preferredColorScheme(.dark)
+    }
 }
