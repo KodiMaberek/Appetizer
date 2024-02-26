821689d19986e85e26e34728b2ea2696dd4a323c
Kodi Maberek <kodi.maberek23@gmail.com>
2024-02-25 22:40:24 -0800

    Order ViewModel

--- Appetizer/Views/Screens/Order/OrderViewModel.swift
+++ Appetizer/Views/Screens/Order/OrderViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 import SwiftUI
 
 final class OrderViewModel: ObservableObject {
+    @AppStorage("order") var saveOrder: Data?
+    
     @Published var order = Order()
     
     func add(_ appertizer: Appetizer) {
@@ -17,5 +19,26 @@ final class OrderViewModel: ObservableObject {
     
     func delete(at offset: IndexSet) {
         order.items.remove(atOffsets: offset)
+        saveOrderList()
+    }
+    
+    func saveOrderList() {
+        let encoded = try? JSONEncoder().encode(order)
+        saveOrder = encoded
+    }
+    
+    func retrivieng() {
+        guard let orderData = saveOrder else { return }
+        
+        do {
+            let decoder = try JSONDecoder().decode(Order.self, from: orderData)
+            order = decoder
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    var totalPrice: Double {
+        order.items.reduce(0) { $0 + $1.price }
     }
 }
